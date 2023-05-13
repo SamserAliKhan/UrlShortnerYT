279f21d710391c755be9e7c7ec5f089bf692f631
SamserAliKhan <sasmeralikhan0448@gmail.com>
2023-05-13 06:13:20 +0530

    new file:   .gitignore 	new file:   models/ShortUrl.js 	new file:   package-lock.json 	new file:   package.json 	new file:   server.js 	new file:   views/index.ejs

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose=require('mongoose')
+const ShortUrl = require('./models/ShortUrl')
+const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/UrlShortner', {
+  userNewUrlParesr:true, useUnifiedTopology: true
+})
+ 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+app.get('/', async(req, res) => {
+  const shortUrls= await ShortUrl.find()
+  res.render('index',{shortUrls:shortUrls})
+})
+
+app.post('/shortUrls', (req, res) => {
+  ShortUrl.create({ful: req.body.fullUrl})
+  res.redirect('/')
+  
+})
+
+app.listen(process.env.PORT || 3000);
